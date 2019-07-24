e3b29800a9da3af5e00538c6a07238ec4a5d8747
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-24 00:37:34 -0400

    Fixes spacing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -18,8 +18,6 @@ var stringifyJSON = function(obj) {
     // join mapped array
     return '[' + result.join(",") + ']';
   }
-  
-
   if (Object.prototype.toString.call(obj) === '[object Object]') {
     let result = [];
     for (let key in obj) {
@@ -35,7 +33,6 @@ var stringifyJSON = function(obj) {
     // join result array
     return '{' + result.join(",") + '}';
   }
-
   // surround strings with quotes
   if (typeof obj === 'string') {
     return '"' + obj + '"';
