fc9dfeec787c3a3f7ada66aed541862c667567b4
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-24 00:35:08 -0400

    Implements stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    // iterate through each array element
+    // map through array with recursive call on each element
+    let result = [];
+    for (let i = 0; i < obj.length; i++) {
+      // skip undefined or functions
+      if (i === undefined || typeof i === 'function') {
+        continue;
+      }
+      result.push(stringifyJSON(obj[i]));
+    }
+    // join mapped array
+    return '[' + result.join(",") + ']';
+  }
+  
+
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    let result = [];
+    for (let key in obj) {
+      // iterate through each obj key
+      // skip undefined or functions
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      // concatenate recursive call on the key, with recursive call on the value
+      // push each concatenated string to result array
+      result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+    }
+    // join result array
+    return '{' + result.join(",") + '}';
+  }
+
+  // surround strings with quotes
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // skip undefined or functions
+  if (obj === undefined || typeof obj === 'function') {
+    return;
+  }
+  return obj + '';
 };
