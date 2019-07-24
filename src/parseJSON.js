8d0e8973db8161238d92525149b160883ddc30a6
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-24 17:29:53 -0400

    Fixes parseJSONSpec.js test suite

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,38 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var parseObj = function (objJSON) {
+
+  };
+
+  var parseArr = function (arrJSON) {
+    if (arrJSON === '[]') {
+      return [];
+    }
+    // split string on commas to create an actual array
+    const arrOfValues = arrJSON.split(',');
+    // return reduction of recursive calls through array
+    return arrOfValues.reduce((acc, currentValue) => {
+      // push each cb return value onto accumulator array
+      acc.push(parseJSON(currentValue));
+      return acc;
+    }, []);
+    
+  };
+  
+  var parseNum = function (numJSON) {
+
+  };
+  
+  var parseStr = function (strJSON) {
+
+  };
+
+  if (json.length === 0) {
+    return '';
+  } else if (json[0] === '[') {
+    return parseArr(json);
+  } else if (json[0] === '{') {
+    return parseObj(json); // pass in copy of obj ??
+  }
 };
